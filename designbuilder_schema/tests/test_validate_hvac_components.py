96856eeda4ec29d85b924f551fd25ea38d17f9b3
Tokarzewski <bartlomiej.tokarzewski@gmail.com>
2024-11-05 17:39:57 +0100

    Update test_validate_hvac_components.py

--- designbuilder_schema/tests/test_validate_hvac_components.py
+++ designbuilder_schema/tests/test_validate_hvac_components.py
@@ -2,13 +2,12 @@ import json
 from designbuilder_schema.hvac_network import HVACComponents
 import pytest
 
-
-@pytest.fixture(
-    params=[
+filepaths = [
         r"C:\GitHub\designbuilder_schema\samples\fragments\DemandSubLoop_HVACComponents.json",
         r"C:\GitHub\designbuilder_schema\samples\fragments\SupplySubLoop_HVACComponents.json",
-    ]
-)
+        ]
+
+@pytest.fixture(params=filepaths)
 def json_filepath(request):
     return request.param
 
@@ -16,10 +15,8 @@ def json_filepath(request):
 def test_validate_component(json_filepath):
     with open(json_filepath, "r") as f:
         json_data = json.load(f)
-        hvac_components = HVACComponents.model_validate(
-            json_data["HVACComponents"]
-        ).HVACComponent
+        hvac_components = HVACComponents.model_validate(json_data["HVACComponents"])
 
-    for hvac_component in hvac_components:
+    for hvac_component in hvac_components.HVACComponent:
         assert hvac_component is not None, f"Invalid HVAC component: {hvac_component}"
         print(hvac_component.type, " - ", hvac_component.__class__.__name__)
