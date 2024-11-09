8fa9149d5ae8117760ea60461c70cb7c2eaf06a1
Tokarzewski <bartlomiej.tokarzewski@gmail.com>
2024-11-09 16:06:26 +0100

    simplify testing

--- designbuilder_schema/tests/test_validate_core.py
+++ designbuilder_schema/tests/test_validate_core.py
@@ -1,7 +1,5 @@
-import os
-from designbuilder_schema.core import DBJSON
-from designbuilder_schema.utils import load_file_to_dict
-import pytest
+import os, pytest
+from designbuilder_schema.utils import load_model
 
 
 def get_filepaths(directory, extension=".json"):
@@ -16,34 +14,20 @@ def get_filepaths(directory, extension=".json"):
 # Get model filepaths from models folder
 models_folder = r".\samples\models"
 json_filepaths = get_filepaths(models_folder, extension=".json")
+xml_filepaths = get_filepaths(models_folder, extension=".xml")
 
 
 @pytest.mark.parametrize("filepath", json_filepaths)
 def test_validate_dbjson(filepath):
-    dictionary = load_file_to_dict(filepath)
-
-    # Check if the "dbJSON" key exists
-    assert "dbJSON" in dictionary, f"Key 'dbJSON' not found in {filepath}"
-
-    db_json = DBJSON.model_validate(dictionary["dbJSON"])
+    db_json = load_model(filepath)
 
     # Check if the validation result is not None
-    assert db_json is not None, f"DBJSON validation failed for {filepath}"
+    assert db_json is not None, f"dbJSON validation failed for {filepath}"
 
 
-# collision in schema - HVACNetwork vs DetailedHVACNetwork
-
-"""
-xml_filepaths = get_filepaths(models_folder, extension=".xml")
 @pytest.mark.parametrize("filepath", xml_filepaths)
 def test_validate_dbxml(filepath):
-    dictionary = load_file_to_dict(filepath)
-
-    ## Check if the "dbJSON" key exists
-    # assert "dbXML" in dictionary, f"Key 'dbXML' not found in {filepath}"
-
-    db_xml = DBJSON.model_validate(dictionary["dbXML"])
+    db_xml = load_model(filepath)
 
     # Check if the validation result is not None
-    assert db_xml is not None, f"DBJSON validation failed for {filepath}"
-"""
+    assert db_xml is not None, f"dbXML validation failed for {filepath}"
