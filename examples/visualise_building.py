cc8b8cd40ee9b72d68a5ab87d88fc4bc7203882b
Tokarzewski <bartlomiej.tokarzewski@gmail.com>
2024-11-19 20:00:18 +0100

    rename

--- examples/visualise_building.py
+++ examples/visualise_building.py
@@ -1,6 +1,7 @@
 from designbuilder_schema.visualise import add_visualisation_extention
 from designbuilder_schema.utils import load_model
 
+
 add_visualisation_extention()
 
 db_json = load_model(r"samples\models\Tutorial Model.xml")
